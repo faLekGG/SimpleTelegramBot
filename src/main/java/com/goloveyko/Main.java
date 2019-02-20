cdbab691b7d4c38fe5c42d699a8088bc7c2b8920
Vitali Halaveika <Vitali.Halaveika@ibm.com>
2019-02-20 15:59:16 +0300

    Initial commit

---
+++ src/main/java/com/goloveyko/Main.java
@@ -0,0 +1,18 @@
+package com.goloveyko;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new MyBot());
+        } catch (TelegramApiException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/main/java/com/goloveyko/MyBot.java
@@ -0,0 +1,44 @@
+package com.goloveyko;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+public class MyBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "HalaveikaBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "723692054:AAHc6cdxudiWHc2mSNXjxMwC_GhVArGA-tE";
+    }
+}
